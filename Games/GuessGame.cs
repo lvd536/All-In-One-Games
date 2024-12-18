39c52a4ebb0e19d66bd08a5fe69df6be10424d8c
lvd. <nbikiti@bk.ru>
2024-12-18 17:52:34 +0400

    Перенос в другой класс

--- Games/GuessGame.cs
+++ Games/GuessGame.cs
@@ -1,10 +1,55 @@
-﻿namespace games.Games;
+﻿using games.Logic;
+using games.Variables;
+
+namespace games.Games;
 
 public class GuessGame
 {
+    private MainVariable mainVar = new MainVariable();
+    private GuessLogic guessLogic = new GuessLogic();
+    private GuessVariables guessVariables = new GuessVariables();
+
     public void Start()
-    { 
+    {
         Console.WriteLine("Добро пожаловать в игру *Угадай число* !");
         Console.WriteLine("Ваша задача: угадать число, которое загадала система");
+    }
+
+    public void StartLogic()
+    {
+        Start();
+        while (true)
+        {
+            Console.WriteLine("Хотите настроить диапазон чисел? (1 - Настроить в ручную | 2 - Оставить базовый)");
+            mainVar.globalInput = Convert.ToInt32(Console.ReadLine());
+            if (mainVar.globalInput == 1)
+            {
+                Console.WriteLine("Введите первое число: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Отлично! Числа будут генерироваться от {num1} до {num2} !");
+                guessLogic.GameLogic(num1, num2, false);
+            }
+
+            else if (mainVar.globalInput == 2) guessLogic.GameLogic(0, 0, true);
+            else
+            {
+            }
+
+            ;
+
+            Console.WriteLine("Хотите повторить игру? Y/N");
+            ConsoleKey input = Console.ReadKey().Key;
+            if (input == ConsoleKey.Y)
+            {
+                guessVariables.userInput = 0;
+            }
+            else if (input == ConsoleKey.N)
+            {
+                break;
+            }
+            else break;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ public class Program
     static void Main(string[] args)
     {
         GuessGame guessGame = new GuessGame();
-        GuessLogic guessLogic = new GuessLogic();
         MainVariable mainVariable = new MainVariable();
-        GuessVariables guessVariables = new GuessVariables();
 
         GameStart();
 
@@ -20,36 +18,8 @@ public class Program
             mainVariable.globalInput = Convert.ToInt32(Console.ReadLine());
             if (mainVariable.globalInput == 1)
             {
-                guessGame.Start();
-                while (true)
-                {
-                    Console.WriteLine("Хотите настроить диапазон чисел? (1 - Настроить в ручную | 2 - Оставить базовый)");
-                    mainVariable.globalInput = Convert.ToInt32(Console.ReadLine());
-                    if (mainVariable.globalInput == 1)
-                    {
-                        Console.WriteLine("Введите первое число: ");
-                        int num1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите второе число: ");
-                        int num2 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine($"Отлично! Числа будут генерироваться от {num1} до {num2} !");
-                        guessLogic.GameLogic(num1, num2, false);
-                    }
-
-                    else if (mainVariable.globalInput == 2) guessLogic.GameLogic(0, 0, true);
-                    else {};
-                    
-                    Console.WriteLine("Хотите повторить игру? Y/N");
-                    ConsoleKey input = Console.ReadKey().Key;
-                    if (input == ConsoleKey.Y)
-                    {
-                        guessVariables.userInput = 0;
-                    }
-                    else if (input == ConsoleKey.N)
-                    {
-                        break;
-                    }
-                    else break;
-                }
+                //guessGame.Start();
+                guessGame.StartLogic();
             }
         }
     }
