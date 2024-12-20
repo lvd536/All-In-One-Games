db901ea59db1ac3f2b96bf37eceba17daee77623
lvd. <nbikiti@bk.ru>
2024-12-20 17:58:50 +0400

    Чистка от говна v1

--- Games/MathGame.cs
+++ Games/MathGame.cs
@@ -5,18 +5,12 @@ namespace games.Games;
 
 public class MathGame
 {
-    MathVariables mathVariables = new MathVariables();
     MathLogic mathLogic = new MathLogic();
-
-    public void Start()
-    {
-        Console.WriteLine("Добро пожаловать в игру *Математическая Викторина* !");
-        Console.WriteLine("Ваша задача: решить пример");
-    }
 
     public void StartLogic()
     {
-        Start();
+        Console.WriteLine("Добро пожаловать в игру *Математическая Викторина* !");
+        Console.WriteLine("Ваша задача: решить пример");
         mathLogic.Launch();
     }
 }
