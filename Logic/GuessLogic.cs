d8b6597f3daa245092af4ac8c87e83660c7041db
lvd. <nbikiti@bk.ru>
2024-12-21 00:06:12 +0400

    Логика сохранения для угадайки

--- Logic/GuessLogic.cs
+++ Logic/GuessLogic.cs
@@ -8,6 +8,7 @@ public class GuessLogic
     private Random rnd = new Random();
     private GuessEarnLogic guessEarn = new GuessEarnLogic();
     private MainVariable mainVar = new MainVariable();
+    private semiDataBaseLogic semiDBLogic = new semiDataBaseLogic();
 
     public void GameLogic(int oneRnd, int twoRnd, bool basic)
     {
@@ -20,17 +21,29 @@ public class GuessLogic
         else guessVar.randomNumber = rnd.Next(oneRnd, twoRnd);
 
         Console.WriteLine("Программа загадала число. Можете начать угадывать");
+        Console.WriteLine(guessVar.randomNumber);
         do
         {
             guessVar.userInput = Convert.ToInt32(Console.ReadLine());
-            guessVar.guesses += 1;
             if (guessVar.userInput == guessVar.randomNumber)
             {
                 Console.WriteLine($"Вы угадали число {guessVar.randomNumber} за {guessVar.guesses} попыток");
                 int coinsEarned = guessEarn.CalculateCoinsEarned(guessVar.guesses, oneRnd, twoRnd);
+                
+                try
+                {
+                    mainVar.globalMoney = Convert.ToInt32(File.ReadAllText(semiDBLogic.folder + "moneyDB.db"));
+                } catch(FormatException){}
                 mainVar.globalMoney += coinsEarned;
-                Console.WriteLine($"Вы заработали {coinsEarned} монет! Всего монет: {mainVar.globalMoney}");
-                guessVar.guesses = 0;
+                File.WriteAllText(semiDBLogic.folder + "moneyDB.db", mainVar.globalMoney.ToString());
+                try
+                {
+                    mainVar.globalGuesses = Convert.ToInt32(File.ReadAllText(semiDBLogic.folder + "guessesDB.db"));
+                }catch(FormatException){}
+                mainVar.globalGuesses ++;
+                File.WriteAllText(semiDBLogic.folder + "guessesDB.db", mainVar.globalGuesses.ToString());
+                
+                Console.WriteLine($"Вы заработали {coinsEarned} монет! Всего монет: {mainVar.globalMoney}. Всего угаданных чисел: {mainVar.globalGuesses}");
             }
             else if (guessVar.userInput < guessVar.randomNumber)
             {
